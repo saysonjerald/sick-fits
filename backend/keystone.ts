779e758ee522e7a6a77cd23a181cd295db41741c
Jerald Sayson <saysonjerald@gmail.com>
2022-04-19 09:42:42 +0800

    Fix: Env file removed

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseUrl = process.env.DATABASE_URL;
 
@@ -36,6 +37,11 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseUrl,
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({ User, Product, ProductImage }),
     ui: {
